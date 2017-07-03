538b3dea285925798ddec1b7f59a08de7adfe945
shyaka <shyaka>
2017-07-03 04:25:28 -0700

    added test five for data type

--- dayone/testprimenumber.py
+++ dayone/testprimenumber.py
@@ -3,7 +3,7 @@ from primenumbers import primenumber
 
 class Testprimenumber(unittest.TestCase):
 
-    def testint(self):
+    def test_int(self):
         result = primenumber('str')
         self.assertEqual(result,'value incorect',  msg = 'value incorect')
 
@@ -15,10 +15,12 @@ class Testprimenumber(unittest.TestCase):
         result = primenumber(11)
         self.assertEqual(result,[1,2,3,5,7,11],msg='invalid output')
 
-    def all_numbers_in_are_Intergers(self):
+    def test_all_numbers_in_are_Intergers(self):
         result =primenumber(34)
         self.assertTrue(all(isinstance(number, int) for number in result), msg="Not all the numberes are integers")
-
+    def test_for_list_being_returned(self):
+        result = primenumber(6)
+        self.assertEqual(type(result), list, msg="wrong data type returned")
 
 if __name__ == '__main__':
     unittest.main()
